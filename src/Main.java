183cb6890a4ca72afca36320a8ebd70189802de8
JoseOrduz <jmorduzc@udistrital.edu.co>
2025-10-13 14:25:38 -0500

    Primer commit: Main principal del programa, falta agregar las demas clases para que funcione.

---
+++ src/Main.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args) {
+        Scanner leer = new Scanner(System.in);
+        GestorCaballeros gestor = new GestorCaballeros();1
+
+        int opcion;
+
+        do {
+            System.out.println("\n=== MENÚ PRINCIPAL ===");
+            System.out.println("1. Validadores");
+            System.out.println("2. Conversores");
+            System.out.println("3. Gestor de Caballeros del Zodiaco");
+            System.out.println("4. Salir");
+            System.out.print("Elija una opción: ");
+            opcion = leer.nextInt();
+
+            switch (opcion) {
+                case 1 -> Validadores.menuValidadores();
+                case 2 -> ConversorMorse.menuMorse();
+                case 3 -> gestor.menuCaballeros();
+                case 4 -> System.out.println("Saliendo del programa...");
+                default -> System.out.println("Opción no válida");
+            }
+
+        } while (opcion != 4);
+    }
+}
