75e76709d3db3ba6dc56561bb24631522a861699
JoseOrduz <jmorduzc@udistrital.edu.co>
2025-10-13 19:42:55 -0500

    cuarto commit: ajuste el main para que tambien tenga el cenversor morse que se me olvido poner jajaja

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,7 @@ import java.util.Scanner;
 public class Main{
     public static void main(String[] args) {
         Scanner leer = new Scanner(System.in);
-        GestorCaballeros gestor = new GestorCaballeros();1
-
+        GestorCaballeros gestor = new GestorCaballeros();
         int opcion;
 
         do {
@@ -17,11 +16,12 @@ public class Main{
 
             switch (opcion) {
                 case 1 -> Validadores.menuValidadores();
-                case 2 -> gestor.menuCaballeros();
-                case 3 -> System.out.println("Saliendo del programa...");
+                case 2 -> ConversorMorse.menuMorse();
+                case 3 -> gestor.menuCaballeros();
+                case 4 -> System.out.println("Saliendo del programa...");
                 default -> System.out.println("Opción no válida");
             }
 
-        } while (opcion != 3);
+        } while (opcion != 4);
     }
 }
